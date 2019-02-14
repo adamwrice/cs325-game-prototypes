4a102b00daa1547203b10ac43a504ce9594949cc
adamwrice <44509408+adamwrice@users.noreply.github.com>
2019-02-14 03:25:41 -0500

    Update main.js

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -4,52 +4,44 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 1600, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
     
     function preload() {
         // Load an image
         game.load.image( 'square', 'assets/square.png' );
         game.load.image( 'triangle', 'assets/triangle.png' );
-        // load a tilemap and call it 'map'.
-        // from .json file
-        game.load.tilemap('map', 'assets/tilemap_example.json', null, Phaser.Tilemap.TILED_JSON);
-        // alternatively, from .csv file
         //game.load.tilemap('map', 'assets/tilemap_example.csv', null, Phaser.Tilemap.CSV);
         
         //load tiles for map
-        game.load.image('tiles', 'assets/tiles.png');
         game.load.audio('music', 'assets/Kai_Engel_-08_-_November.mp3');
+        
+        game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('tiles-1', 'assets/tiles-1.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        game.load.spritesheet('droid', 'assets/droid.png', 32, 32);
+        game.load.image('starSmall', 'assets/star.png');
+        game.load.image('starBig', 'assets/star2.png');
+        game.load.image('background', 'assets/background2.png');
     }
     
-    var map;
     var layer1;
     var square;
     var triangle;
     var music;
+    var map;
+    var tileset;
+    var layer;
+    var player;
+    var facing = 'left';
+    var jumpTimer = 0;
+    var cursors;
+    var jumpButton;
+    var bg;
     
     function create() {
         music = game.add.audio('music');
         music.loopFull(0.6);
-        // Create the map. 
-        map = game.add.tilemap('map');
-        // for csv files specify the tile size.
-        //map = game.add.tilemap('map', 32, 32);
         
-        //add tiles
-        map.addTilesetImage('tiles');
-        
-        // Create a layer from the map
-        //using the layer name given in the .json file
-        layer1 = map.createLayer('Tile Layer 1');
-        //for csv files
-        //layer1 = map.createLayer(0);
-        
-        //  Resize the world
-        layer1.resizeWorld();
-        
-        //Create a sprite
-        square = game.add.sprite( game.world.centerX, game.world.centerY, 'square.png' );
-        square.anchor.setTo( 0.5, 0.5 );
         triangle = game.add.sprite( game.world.centerX, game.world.centerY, 'triangle.png' );
         triangle.anchor.setTo(0.5, 0.5);
         
@@ -57,21 +49,110 @@ window.onload = function() {
         game.physics.enable( square, Phaser.Physics.ARCADE );
         game.physics.enable( triangle, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
-        square.body.collideWorldBounds = true;
         triangle.body.collideWorldBounds = true;
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         //var text = game.add.text( 400, 15, "Build something amazing.", style );
         //text.fixedToCamera = true;
         //text.anchor.setTo( 0.5, 0.0 );
         
-        game.camera.follow(square);
+      
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        game.stage.backgroundColor = '#000000';
+
+        bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+        bg.fixedToCamera = true;
+
+        map = game.add.tilemap('level1');
+
+        map.addTilesetImage('tiles-1');
+
+        map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+
+        layer = map.createLayer('Tile Layer 1');
+
+        //  Un-comment this on to see the collision tiles
+        // layer.debug = true;
+
+        layer.resizeWorld();
+
+        game.physics.arcade.gravity.y = 250;
+
+        player = game.add.sprite(32, 32, 'square');
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+
+        player.body.bounce.y = 0.2;
+        player.body.collideWorldBounds = true;
+        player.body.setSize(20, 32, 5, 16);
+
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('turn', [4], 20, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+        game.camera.follow(player);
+        
+
+        cursors = game.input.keyboard.createCursorKeys();
+        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
     }
     
     function update() {
-        
-    }
+        game.physics.arcade.collide(player, layer);
+
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown)
+        {
+          player.body.velocity.x = -150;
+
+          if (facing != 'left')
+         {
+               player.animations.play('left');
+               facing = 'left';
+            }
+      }
+        else if (cursors.right.isDown)
+        {
+            player.body.velocity.x = 150;
+
+            if (facing != 'right')
+            {
+                player.animations.play('right');
+                facing = 'right';
+            }
+        }
+        else
+        {
+            if (facing != 'idle')
+            {
+                player.animations.stop();
+
+                if (facing == 'left')
+                {
+                    player.frame = 0;
+                }
+                else
+                {
+                    player.frame = 5;
+                }
+
+                facing = 'idle';
+            }
+        }
+    
+        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+        {
+            player.body.velocity.y = -250;
+            jumpTimer = game.time.now + 750;
+        }
+ }
+function render () {
+
+    // game.debug.text(game.time.physicsElapsed, 32, 32);
+    // game.debug.body(player);
+    // game.debug.bodyInfo(player, 16, 24);
+
+}
 };
