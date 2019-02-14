425785696d32efcb0535a03e00f0902be23c3536
adamwrice <44509408+adamwrice@users.noreply.github.com>
2019-02-14 12:49:48 -0500

    Update main.js

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -25,16 +25,18 @@ window.onload = function() {
     }
     
     var layer1;
-    var triangle;
     var music;
     var map;
     var tileset;
     var layer;
     var player;
+    var player2;
     var facing = 'left';
     var jumpTimer = 0;
+    var jumpTimer2 = 0;
     var cursors;
     var jumpButton;
+    var jumpButton2;
     var bg;
     
     function create() {
@@ -71,22 +73,32 @@ window.onload = function() {
         player.animations.add('turn', [4], 20, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+        player2 = game.add.sprite(32, 32, 'dude');
+        game.physics.enable(player2, Phaser.Physics.ARCADE);
+
+        player2.body.bounce.y = 0.2;
+        player2.body.collideWorldBounds = true;
+        player2.body.setSize(20, 32, 5, 16);
+
+        player2.animations.add('left', [0, 1, 2, 3], 10, true);
+        player2.animations.add('turn', [4], 20, true);
+        player2.animations.add('right', [5, 6, 7, 8], 10, true);
         game.camera.follow(player);
         
 
         cursors = game.input.keyboard.createCursorKeys();
         jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        
+        jumpButton2 = game.input.keyboard.addKey(Phaser.Keyboard.w);
         music = game.add.audio('music');
         music.play();
         
-        triangle = game.add.sprite( game.world.centerX, game.world.centerY, 'triangle.png' );
-        triangle.anchor.setTo(0.5, 0.5);
+        //triangle = game.add.sprite( game.world.centerX, game.world.centerY, 'triangle.png' );
+        //triangle.anchor.setTo(0.5, 0.5);
         
         // Turn on the arcade physics engine for this sprite.
         //game.physics.enable(triangle, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
-        triangle.body.collideWorldBounds = true;
+        //triangle.body.collideWorldBounds = true;
         
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         //var text = game.add.text( 400, 15, "Build something amazing.", style );
@@ -103,20 +115,40 @@ window.onload = function() {
         if (cursors.left.isDown)
         {
           player.body.velocity.x = -150;
-
-          if (facing != 'left')
-         {
-               player.animations.play('left');
-               facing = 'left';
-            }
-      }
+        }
         else if (cursors.right.isDown)
         {
             player.body.velocity.x = 150;
+        }
+        
+    
+        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+        {
+            player.body.velocity.y = -250;
+            jumpTimer = game.time.now + 750;
+        }
+        
+        game.physics.arcade.collide(player2, layer);
+
+        player2.body.velocity.x = 0;
+
+        if (cursors.a.isDown)
+        {
+          player2.body.velocity.x = -150;
+
+          if (facing != 'left')
+         {
+               player2.animations.play('left');
+               facing = 'left';
+            }
+      }
+        else if (cursors.d.isDown)
+        {
+            player2.body.velocity.x = 150;
 
             if (facing != 'right')
             {
-                player.animations.play('right');
+                player2.animations.play('right');
                 facing = 'right';
             }
         }
@@ -124,25 +156,25 @@ window.onload = function() {
         {
             if (facing != 'idle')
             {
-                player.animations.stop();
+                player2.animations.stop();
 
                 if (facing == 'left')
                 {
-                    player.frame = 0;
+                    player2.frame = 0;
                 }
                 else
                 {
-                    player.frame = 5;
+                    player2.frame = 5;
                 }
 
                 facing = 'idle';
             }
         }
     
-        if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+        if (jumpButton2.isDown && player2.body.onFloor() && game.time.now > jumpTimer2)
         {
-            player.body.velocity.y = -250;
-            jumpTimer = game.time.now + 750;
+            player2.body.velocity.y = -250;
+            jumpTimer2 = game.time.now + 750;
         }
  }
 function render () {
