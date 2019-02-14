ade616bc04e93701a47b1f88a42e962a58f58127
adamwrice <44509408+adamwrice@users.noreply.github.com>
2019-02-14 03:05:44 -0500

    Update main.js

--- HW2/js/main.js
+++ HW2/js/main.js
@@ -2,18 +2,9 @@ window.onload = function() {
     // You might want to start with a template that uses GameStates:
     //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
     "use strict";
     
-    var game = new Phaser.Game( 600, 1600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1600, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
         // Load an image and call it 'logo'.
@@ -26,14 +17,18 @@ window.onload = function() {
         
         //load tiles for map
         game.load.image('tiles', 'assets/tiles.png');
+        game.load.audio('music', 'assets/Kai_Engel_-08_-_November.mp3');
     }
     
     var map;
     var layer1;
     var square;
     var triangle;
+    var music;
     
     function create() {
+        music = game.add.audio('music');
+        music.loopFull(0.6);
         // Create the map. 
         map = game.add.tilemap('map');
         // for csv files specify the tile size.
