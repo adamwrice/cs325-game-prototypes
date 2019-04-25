5b361e15280ed1921eaf530094cf8f1f77b20558
adamwrice <44509408+adamwrice@users.noreply.github.com>
2019-04-25 14:22:48 -0400

    Update Game.js

--- Mech Miner (2)/js/Game.js
+++ Mech Miner (2)/js/Game.js
@@ -164,7 +164,7 @@ GameStates.makeGame = function( game, shared ) {
             game.physics.arcade.enable(sprite);
 
             //  This adjusts the collision body size.
-            sprite.body.setSize(90, 40, 0, 0);
+            sprite.body.setSize(50, 50, 0, 0);
 
             //  We'll set a lower max angular velocity here to keep it from going totally nuts
             sprite.body.maxAngular = 500;
